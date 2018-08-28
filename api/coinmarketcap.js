f10b4916888b39bb3bc9d5a229eec8486068b32d
Mats J Skaslien <matsjorgen@hotmail.com>
2018-08-28 15:50:08 +0200

    adds get info by coin function

--- api/coinmarketcap.js
+++ api/coinmarketcap.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
+const keystore = require('./cmc_key.js') 
+// TODO: set this environment variable
 
 /**
- * 
+ * DEPRECATED
  * @param {*} top_n 
  * @param {*} exclude You probably want to exclude USDT
  */
 async function cmc_top_by_mcap (top_n, exclude = []) {
     const response = await axios.get(`https://api.coinmarketcap.com/v2/ticker/?limit=${top_n}`)
-    let top_coins = {};
+    let top_coins = {};r
     for (const [key, value] of Object.entries(response.data.data)) {
         top_coins[value.symbol] = {'name': value.name, 'mcap': value.quotes['USD']['market_cap']}
     };
@@ -16,6 +18,16 @@ async function cmc_top_by_mcap (top_n, exclude = []) {
     });
 }
 
+
+async function get_coin_data (tickers) {
+    const response = await axios.get(
+        `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${tickers}`,
+        { headers : { 'X-CMC_PRO_API_KEY' : keystore.key } }
+        )
+    return response.data.data 
+}
+
 module.exports = {
-    cmc_top_by_mcap
+    cmc_top_by_mcap,
+    get_coin_data
 }
