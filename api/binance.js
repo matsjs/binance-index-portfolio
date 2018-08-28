1ebf4c5e57c4cb535a8b7171548dcda697410e59
Mats J Skaslien <matsjorgen@hotmail.com>
2018-08-28 14:25:08 +0200

    adds pair retrieval from binance

--- api/binance.js
+++ api/binance.js
@@ -1,1 +1,8 @@
 const axios = require('axios');
+
+async function get_all_btc_pairs() {
+    const response = await axios.get(`https://api.binance.com/api/v3/ticker/bookTicker`)
+    return response.data.filter(
+        pair => pair.symbol.substring(pair.symbol.length - 3) == 'BTC'
+    );
+}
